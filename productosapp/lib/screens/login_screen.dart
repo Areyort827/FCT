1eba93fc492d331e34a1ee0b1043e815d20f8cf5
Areyort827 <Areyort827@iesmartinezm.es>
2023-03-30 12:56:42 +0000

    flutter

---
+++ productosapp/lib/screens/login_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:productosapp/widgets/widgets.dart';
+
+class LoginScreen extends StatelessWidget {
+  //const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: AuthBackground());
+  }
+}
---
+++ productosapp/lib/widgets/widgets.dart
@@ -0,0 +1,1 @@
+export 'package:productosapp/widgets/auth_background.dart';
